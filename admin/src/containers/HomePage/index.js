62c718c8358b1807f460ea3b5a32a548337573f5
Marco Padillo <marco@cynder.io>
2020-12-16 21:59:17 +0800

    feat: plugin page header

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -4,10 +4,25 @@
  *
  */
 
-import { memo } from 'react';
+import React, { memo } from 'react';
+
+import { Header } from '@buffetjs/custom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	padding: 18px 30px;
+`;
 
 const HomePage = () => {
-	return null;
+	return (
+		<Container>
+			<Header
+				title={{
+					label: 'CMS',
+				}}
+			/>
+		</Container>
+	);
 };
 
 export default memo(HomePage);
