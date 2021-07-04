93787183c42e99c27311e6661d3ca99d395169ff
Marco Padillo <marco@cynder.io>
2021-07-04 12:17:40 +0800

    feat: define controllers

--- controllers/category.js
+++ controllers/category.js
@@ -1,23 +1,90 @@
+/* eslint-disable no-underscore-dangle */
 const { isNumber } = require('lodash');
-const { sanitizeEntity } = require('strapi-utils');
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+const pluginId = require('../admin/src/pluginId');
 
 module.exports = {
-	/**
-	 * Retrieve a record.
-	 *
-	 * @return {Object}
-	 */
+	async find(ctx) {
+		let entities;
+
+		if (ctx.query._q) {
+			entities = await strapi.plugins[pluginId].services.category.search(ctx.query);
+		} else {
+			entities = await strapi.plugins[pluginId].services.category.find(ctx.query);
+		}
+
+		return entities.map((entity) =>
+			sanitizeEntity(entity, {
+				model: strapi.plugins[pluginId].models.category,
+			}),
+		);
+	},
 
 	async findOne(ctx) {
 		const { id } = ctx.params;
 		let entity;
 
 		if (isNumber(id)) {
-			entity = await strapi.services.category.findOne({ id });
+			entity = await strapi.plugins[pluginId].services.category.findOne({ id });
 		} else {
-			entity = await strapi.services.category.findOne({ slug: id });
+			entity = await strapi.plugins[pluginId].services.category.findOne({ slug: id });
 		}
 
-		return sanitizeEntity(entity, { model: strapi.models.category });
+		return sanitizeEntity(entity, { model: strapi.plugins[pluginId].models.category });
+	},
+
+	async count(ctx) {
+		if (ctx.query._q) {
+			return strapi.plugins[pluginId].services.category.countSearch(ctx.query);
+		}
+		return strapi.plugins[pluginId].services.category.count(ctx.query);
+	},
+
+	async create(ctx) {
+		let entity;
+		if (ctx.is('multipart')) {
+			const { data, files } = parseMultipartData(ctx);
+			entity = await strapi.plugins[pluginId].services.category.create(data, {
+				files,
+			});
+		} else {
+			entity = await strapi.plugins[pluginId].services.category.create(ctx.request.body);
+		}
+		return sanitizeEntity(entity, {
+			model: strapi.plugins[pluginId].models.category,
+		});
+	},
+
+	async update(ctx) {
+		const { id } = ctx.params;
+
+		let entity;
+		if (ctx.is('multipart')) {
+			const { data, files } = parseMultipartData(ctx);
+			entity = await strapi.plugins[pluginId].services.category.update({ id }, data, {
+				files,
+			});
+		} else {
+			entity = await strapi.plugins[pluginId].services.category.update(
+				{ id },
+				ctx.request.body,
+			);
+		}
+
+		return sanitizeEntity(entity, {
+			model: strapi.plugins[pluginId].models.category,
+		});
+	},
+
+	async delete(ctx) {
+		const { id } = ctx.params;
+
+		const entity = await strapi.plugins[pluginId].services.category.delete({
+			id,
+		});
+		return sanitizeEntity(entity, {
+			model: strapi.plugins[pluginId].models.category,
+		});
 	},
 };
--- controllers/media-item.js
+++ controllers/media-item.js
@@ -1,6 +1,90 @@
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+/* eslint-disable no-underscore-dangle */
+const { isNumber } = require('lodash');
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+const pluginId = require('../admin/src/pluginId');
+
+module.exports = {
+	async find(ctx) {
+		let entities;
+
+		if (ctx.query._q) {
+			entities = await strapi.plugins[pluginId].services['media-item'].search(ctx.query);
+		} else {
+			entities = await strapi.plugins[pluginId].services['media-item'].find(ctx.query);
+		}
+
+		return entities.map((entity) =>
+			sanitizeEntity(entity, {
+				model: strapi.plugins[pluginId].models['media-item'],
+			}),
+		);
+	},
+
+	async findOne(ctx) {
+		const { id } = ctx.params;
+		let entity;
+
+		if (isNumber(id)) {
+			entity = await strapi.plugins[pluginId].services['media-item'].findOne({ id });
+		} else {
+			entity = await strapi.plugins[pluginId].services['media-item'].findOne({ slug: id });
+		}
+
+		return sanitizeEntity(entity, { model: strapi.plugins[pluginId].models['media-item'] });
+	},
+
+	async count(ctx) {
+		if (ctx.query._q) {
+			return strapi.plugins[pluginId].services['media-item'].countSearch(ctx.query);
+		}
+		return strapi.plugins[pluginId].services['media-item'].count(ctx.query);
+	},
+
+	async create(ctx) {
+		let entity;
+		if (ctx.is('multipart')) {
+			const { data, files } = parseMultipartData(ctx);
+			entity = await strapi.plugins[pluginId].services['media-item'].create(data, {
+				files,
+			});
+		} else {
+			entity = await strapi.plugins[pluginId].services['media-item'].create(ctx.request.body);
+		}
+		return sanitizeEntity(entity, {
+			model: strapi.plugins[pluginId].models['media-item'],
+		});
+	},
+
+	async update(ctx) {
+		const { id } = ctx.params;
+
+		let entity;
+		if (ctx.is('multipart')) {
+			const { data, files } = parseMultipartData(ctx);
+			entity = await strapi.plugins[pluginId].services['media-item'].update({ id }, data, {
+				files,
+			});
+		} else {
+			entity = await strapi.plugins[pluginId].services['media-item'].update(
+				{ id },
+				ctx.request.body,
+			);
+		}
+
+		return sanitizeEntity(entity, {
+			model: strapi.plugins[pluginId].models['media-item'],
+		});
+	},
+
+	async delete(ctx) {
+		const { id } = ctx.params;
+
+		const entity = await strapi.plugins[pluginId].services['media-item'].delete({
+			id,
+		});
+		return sanitizeEntity(entity, {
+			model: strapi.plugins[pluginId].models['media-item'],
+		});
+	},
+};
