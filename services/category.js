89fca8932334d3e007a1509f9da906a76aaf4cc5
Marco Padillo <marco@cynder.io>
2021-07-04 14:48:15 +0800

    feat: service functions

--- services/category.js
+++ services/category.js
@@ -1,6 +1,72 @@
+const pluginId = require('../admin/src/pluginId');
+
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+	find(params, populate) {
+		return strapi.query('category', pluginId).find(params, populate);
+	},
+
+	findOne(params, populate) {
+		return strapi.query('category', pluginId).findOne(params, populate);
+	},
+
+	count(params) {
+		return strapi.query('category', pluginId).count(params);
+	},
+
+	async create(data, { files } = {}) {
+		const validData = await strapi.entityValidator.validateEntityCreation(
+			strapi.plugins[pluginId].category,
+			data,
+		);
+		const entry = await strapi.query('category', pluginId).create(validData);
+
+		if (files) {
+			// automatically uploads the files based on the entry and the model
+			await strapi.entityService.uploadFiles(entry, files, {
+				model: 'category',
+				source: pluginId,
+				// if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+			});
+			return this.findOne({ id: entry.id });
+		}
+
+		return entry;
+	},
+
+	async update(params, data, { files } = {}) {
+		const validData = await strapi.entityValidator.validateEntityUpdate(
+			strapi.plugins[pluginId].category,
+			data,
+		);
+		const entry = await strapi.query('category', pluginId).update(params, validData);
+
+		if (files) {
+			// automatically uploads the files based on the entry and the model
+			await strapi.entityService.uploadFiles(entry, files, {
+				model: 'category',
+				source: pluginId,
+				// if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+			});
+			return this.findOne({ id: entry.id });
+		}
+
+		return entry;
+	},
+
+	delete(params) {
+		return strapi.query('category', pluginId).delete(params);
+	},
+
+	search(params) {
+		return strapi.query('category', pluginId).search(params);
+	},
+
+	countSearch(params) {
+		return strapi.query('category', pluginId).countSearch(params);
+	},
+};
--- services/media-item.js
+++ services/media-item.js
@@ -1,4 +1,70 @@
+const pluginId = require('../admin/src/pluginId');
+
 module.exports = {
+	find(params, populate) {
+		return strapi.query('media-item', pluginId).find(params, populate);
+	},
+
+	findOne(params, populate) {
+		return strapi.query('media-item', pluginId).findOne(params, populate);
+	},
+
+	count(params) {
+		return strapi.query('media-item', pluginId).count(params);
+	},
+
+	async create(data, { files } = {}) {
+		const validData = await strapi.entityValidator.validateEntityCreation(
+			strapi.plugins[pluginId]['media-item'],
+			data,
+		);
+		const entry = await strapi.query('media-item', pluginId).create(validData);
+
+		if (files) {
+			// automatically uploads the files based on the entry and the model
+			await strapi.entityService.uploadFiles(entry, files, {
+				model: 'media-item',
+				source: pluginId,
+				// if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+			});
+			return this.findOne({ id: entry.id });
+		}
+
+		return entry;
+	},
+
+	async update(params, data, { files } = {}) {
+		const validData = await strapi.entityValidator.validateEntityUpdate(
+			strapi.plugins[pluginId]['media-item'],
+			data,
+		);
+		const entry = await strapi.query('media-item', pluginId).update(params, validData);
+
+		if (files) {
+			// automatically uploads the files based on the entry and the model
+			await strapi.entityService.uploadFiles(entry, files, {
+				model: 'media-item',
+				source: pluginId,
+				// if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+			});
+			return this.findOne({ id: entry.id });
+		}
+
+		return entry;
+	},
+
+	delete(params) {
+		return strapi.query('media-item', pluginId).delete(params);
+	},
+
+	search(params) {
+		return strapi.query('media-item', pluginId).search(params);
+	},
+
+	countSearch(params) {
+		return strapi.query('media-item', pluginId).countSearch(params);
+	},
+
 	sanitizeForPublic(mediaItem) {
 		const { author, ...rest } = mediaItem;
 
